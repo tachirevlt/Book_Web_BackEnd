dbbc35880006ccfdd8111edfb8f2572b0b65898b
xcl2406 <lehieu122004@gmail.com>
2025-10-28 16:13:58 +0700

    update

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49feb1dd2eb22fa782a59561e247e4cc5138a56e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cd58113fd0c98340e27a141398726bdcaf51c05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Infrastructure/Migrations/20251028090449_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    authors = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    year = table.Column<double>(type: "float", nullable: true),
+                    books_count = table.Column<int>(type: "int", nullable: true),
+                    work_id = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    isbn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    language_code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    average_rating = table.Column<double>(type: "float", nullable: true),
+                    ratings = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
